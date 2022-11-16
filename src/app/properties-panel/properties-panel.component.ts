47acc77a9bf9a5e6f9715f3c61a8ededd6b198d5
Nithya <nitya.tai@gmail.com>
2022-11-16 14:31:39 +0530

    changes after pr

--- src/app/properties-panel/properties-panel.component.ts
+++ src/app/properties-panel/properties-panel.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { properties } from '../store/our.model';
 import { PropserviceService } from '../services/propservice.service';
 import { EventsService } from '../services/events.service';
-
 @Component({
   selector: 'app-properties-panel',
   templateUrl: './properties-panel.component.html',
@@ -10,7 +9,6 @@ import { EventsService } from '../services/events.service';
 })
 export class PropertiesPanelComponent implements OnInit {
   isdisabled:boolean = this.PropserviceService.isdisabled; 
-
   constructor(private PropserviceService:PropserviceService,private eventservice:EventsService) {
      this.PropserviceService.subjectforProp$.subscribe(
     (Properties) => {console.log(Properties+' properties')
@@ -23,6 +21,7 @@ export class PropertiesPanelComponent implements OnInit {
     });
   }
   ngOnInit(): void { }
+
   PropertyPanelValues: properties = {
     strokewidth: 1,
     strokecolor: '#999999',
@@ -33,27 +32,22 @@ export class PropertiesPanelComponent implements OnInit {
 
   thumbLabel = true;
 
-  onSliderChange(value: number) {
-    this.PropertyPanelValues.strokewidth = (value);
+  onPropChange(prop:string,value: string) {
+    if(prop=='strokewidth'){
+      this.PropertyPanelValues.strokewidth = Number(value);
+      this.PropserviceService.onPropertyPanelChange(this.PropertyPanelValues);
+    }
+    else if(prop=='strokecolor'){
+      this.PropertyPanelValues.strokecolor = value;
+      this.PropserviceService.onPropertyPanelChange(this.PropertyPanelValues);
+    }
+    else if(prop=='fillcolor'){
+      this.PropertyPanelValues.fillcolor = value;
+      this.PropserviceService.onPropertyPanelChange(this.PropertyPanelValues);
+    }
+    else if(prop=='angle'){
+      this.PropertyPanelValues.objangle = Number(value);
     this.PropserviceService.onPropertyPanelChange(this.PropertyPanelValues)
-    
+    }
   }
-  onStrokeColorChange(value: string) {
-    this.PropertyPanelValues.strokecolor = value;
-    this.PropserviceService.onPropertyPanelChange(this.PropertyPanelValues)
-  }
-
-  onFillColorChange(value: string) {
-    this.PropertyPanelValues.fillcolor = value;
-    this.PropserviceService.onPropertyPanelChange(this.PropertyPanelValues)
-  }
-  onAngleChange(value:string) {
-    this.PropertyPanelValues.objangle = Number(value);
-    this.PropserviceService.onPropertyPanelChange(this.PropertyPanelValues)
-  
-  }
-
-
-  
-
 }
